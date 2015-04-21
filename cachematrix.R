6c9d19b2c38ddc5e600a7bfdbf567a7f617985a4
Hermarchus <vladvolkov239@gmail.com>
2015-04-22 00:37:34 +0300

    Completing the task.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This "package" defines an "object type" CacheMatrix which is matrix
+## with caching inverse calculation optimization. It provides functions
+## to create such matrix from usual matrix and calculate inverse with caching.
 
-## Write a short comment describing this function
+## Creating CacheMatrix object from a given matrix x which is able to 
+## cache inverse matrix.
+## Parameters:
+## x    --- given matrix.
+## Variables:
+## x    --- represents the matrix.
+## inv  --- represents the inverse matrix.
+## Functions:
+## set/get functions for the above variables.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    #Setting up basic set/get functions.
+
+    inv  <- NULL
+    set    <- function(y) {
+        x   <<- y
+        inv <<- NULL #forgetting outdated inverse.
+    }
+    get    <- function() {
+        x
+    }
+    setinv <- function(inverse) {
+        inv <<- inverse
+    }
+    getinv <- function() {
+        inv
+    }
+
+    #Creating CacheMatrix object as a list of the above functions.
+
+    list(set    = set, 
+         get    = get,
+         setinv = setinv,
+         getinv = getinv)
+
 }
 
+## Calculating inverse matrix of the CacheMatrix object and caches it.
+## If it's already calculated returns cached value.
+## Parameters:
+## x    --- CacheMatrix object.
+## safe --- if true, cached value is checked for validity before return
+## [in case there is possibility of direct access to matrix of x through,
+## for example, environment(x$get)$x = ...].
+## ...  --- parameters for solve function to find inverse.
 
-## Write a short comment describing this function
+cacheSolve <- function(x, safe = FALSE, ...) {
+    
+    #Getting matrix and its possible inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv  <- x$getinv()
+    xnow <- x$get()
+
+    #Checking validity of cached inverse, and return it if it is valid.
+
+    if (!is.null(inv)) {
+
+        #Performing check and returning cached value.
+
+        if (!safe | all.equal(xnow %*% inv, diag(nrow(xnow)))) {
+            message('Returning cached data.')
+            return(inv)
+        } else {
+            message('Cached data is outdated: recalculating.')
+        }
+
+    }
+
+    #Recalulating inverse matrix and caching it.
+
+    inv <- solve(xnow, ...)
+    x$setinv(inv)
+    inv
 }
